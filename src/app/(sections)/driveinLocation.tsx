9bd8fce09652d45b3f0a229a445669e844062924
leonardof <leonardof@ctir.gov.br>
2024-09-09 11:11:51 -0300

    update loc image

--- src/app/(sections)/driveinLocation.tsx
+++ src/app/(sections)/driveinLocation.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import SectionContainer from "../../containers/sectionContainer";
-import Image from "next/image";
-import location from "../../../public/images/loc.png";
+import location from "../../../public/images/loc-v2.png";
+import locationSmall from "../../../public/images/loc-v3.png";
 
 export default function DriveinLocation() {
   return (
@@ -16,18 +16,18 @@ export default function DriveinLocation() {
           href="https://www.google.com/maps?ll=-15.778598,-47.897692&z=18&t=m&hl=en-AU&gl=US&mapclient=embed&cid=18109967453310915620"
           target="_blank"
           rel="noopener noreferrer"
-          className="w-11/12 sm:w-10/12 max-w-[1200px] h-[300px] rounded-lg  shadow-md relative overflow-hidden"
+          className="w-11/12 sm:w-10/12 max-w-[970px] h-[317px] rounded-lg  shadow-md relative overflow-hidden"
         >
-          {/* <Image
-            src={location.src}
-            layout="fill"
-            className="w-full h-full bg-center hover:cursor-pointer rounded-lg bg-cover bg-no-repeat transition-transform duration-300 ease-in-out transform hover:scale-110"
-            alt="location"
-          /> */}
           <div
-            className="w-full h-full bg-center hover:cursor-pointer rounded-lg bg-cover bg-no-repeat transition-transform duration-300 ease-in-out transform hover:scale-110"
+            className="hidden md:block w-full h-full bg-center hover:cursor-pointer rounded-lg bg-cover bg-no-repeat transition-transform duration-300 ease-in-out transform hover:scale-105"
             style={{
               backgroundImage: `url(${location.src})`,
+            }}
+          />
+          <div
+            className="block md:hidden w-full h-full bg-center hover:cursor-pointer rounded-lg bg-cover bg-no-repeat transition-transform duration-300 ease-in-out transform hover:scale-105"
+            style={{
+              backgroundImage: `url(${locationSmall.src})`,
             }}
           />
         </a>
