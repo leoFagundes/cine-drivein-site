fec24f7d9ffed193c2fc9389d037d2422f634f46
leoFagundes <leofagundes2015@gmail.com>
2024-09-14 15:55:42 -0300

    update details

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -58,54 +58,59 @@ export default function Header() {
   ];
 
   return (
-    <nav className="relative flex items-center justify-between h-20 my-4 w-11/12 sm:w-10/12 max-w-[1200px]">
-      <Link href={"/"}>
-        {" "}
-        <Image src={"/svg/logo.svg"} width={120} height={60} alt="logo" />
-      </Link>
+    <>
+      <div className="h-[80px]" />
+      <section className="fixed flex justify-center w-full z-40 bg-primary/85 backdrop-blur-sm">
+        <nav className="flex items-center justify-between h-20 py-4 w-11/12 sm:w-10/12 max-w-[1200px]">
+          <Link href={"/"}>
+            {" "}
+            <Image src={"/svg/logo.svg"} width={120} height={60} alt="logo" />
+          </Link>
 
-      <ul className="items-center gap-7 font-semibold text-xs hidden lg:flex">
-        {items.map((item) => (
-          <li
-            key={item.key}
-            className="text-xs hover:opacity-70"
-            onClick={() => setOpened(false)}
-          >
-            <Link href={item.link}>{item.label}</Link>
-          </li>
-        ))}
-      </ul>
-      <div
-        onClick={() => setOpened(!opened)}
-        className={classNames(
-          `lg:hidden block tham tham-e-squeeze tham-w-6 scale-110 hover:opacity-100 md:hover:opacity-80`,
-          {
-            "tham-active": opened,
-          }
-        )}
-      >
-        <div className="tham-box">
-          <div className="tham-inner bg-gray" />
-        </div>
-      </div>
-      {opened && (
-        <div className="absolute lg:hidden block right-0 bottom-0 translate-y-40 p-3 border border-gray/50 rounded-md z-10 bg-primary shadow-xl">
-          <ul className="flex flex-col gap-3 font-semibold">
+          <ul className="items-center gap-7 font-semibold text-xs hidden lg:flex">
             {items.map((item) => (
               <li
                 key={item.key}
-                className="text-xs"
+                className="text-xs hover:opacity-70"
                 onClick={() => setOpened(false)}
               >
-                <Link className="flex gap-2 items-center" href={item.link}>
-                  {item.icon}
-                  {item.label}
-                </Link>
+                <Link href={item.link}>{item.label}</Link>
               </li>
             ))}
           </ul>
-        </div>
-      )}
-    </nav>
+          <div
+            onClick={() => setOpened(!opened)}
+            className={classNames(
+              `lg:hidden block tham tham-e-squeeze tham-w-6 scale-110 hover:opacity-100 md:hover:opacity-80`,
+              {
+                "tham-active": opened,
+              }
+            )}
+          >
+            <div className="tham-box">
+              <div className="tham-inner bg-gray" />
+            </div>
+          </div>
+          {opened && (
+            <div className="absolute lg:hidden block right-0 bottom-0 translate-y-40 p-3 border border-gray/50 rounded-md z-10 bg-primary shadow-xl">
+              <ul className="flex flex-col gap-3 font-semibold">
+                {items.map((item) => (
+                  <li
+                    key={item.key}
+                    className="text-xs"
+                    onClick={() => setOpened(false)}
+                  >
+                    <Link className="flex gap-2 items-center" href={item.link}>
+                      {item.icon}
+                      {item.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </nav>
+      </section>
+    </>
   );
 }
